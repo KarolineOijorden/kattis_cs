b16b1fb3537a4a98c1bbd42b4d76aef45d1cebd3
Karoline Øijorden <karolineoij@gmail.com>
2022-02-02 17:41:53 +0100

    add carrots.cs

---
+++ kattis_cs/carrots.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace kattis_cs
+{
+    class carrots
+    {
+        static void Main(string [] args)
+        {
+            string [] contestants = Console.ReadLine().Split();
+            Console.WriteLine(contestants[1]);
+        }
+    }
+}
