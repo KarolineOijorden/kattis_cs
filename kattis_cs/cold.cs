4eecb0ef2d40f58b9451ca708e03e38664349e33
Karoline Øijorden <karolineoij@gmail.com>
2022-02-02 16:55:58 +0100

    add inital files

---
+++ kattis_cs/cold.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace kattiscs
+{
+    class Cold
+    {
+        static void Main(string[] args)
+        {
+            int nTemperatures = int.Parse(Console.ReadLine());
+            int[] temperatures = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int colddays = 0;
+
+            for (int i = 0; i < nTemperatures; i++)
+            {
+                if (temperatures[i] < 0)
+                {
+                    colddays++;
+                }
+            }
+            Console.WriteLine(colddays);
+        }
+    }
+}
