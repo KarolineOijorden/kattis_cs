44df67804a4810e44353ed1d94db762a83b54aab
Karoline Øijorden <karolineoij@gmail.com>
2022-02-04 13:50:36 +0100

    add Sorttwonumbers and Numberfun

---
+++ kattis_cs/Numberfun.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis_cs
+{
+    class Numberfun
+    {
+        static void Main(string [] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                double[] numbers = Array.ConvertAll(Console.ReadLine().Split(), s => double.Parse(s));
+                if (numbers[0] + numbers[1] == numbers[2] || numbers[0] - numbers[1] == numbers[2]
+                    || numbers[1] - numbers[0] == numbers[2] || numbers[0] * numbers[1] == numbers[2]
+                    || numbers[0] / numbers[1] == numbers[2] || numbers[1] / numbers[0] == numbers[2])
+                {
+                    Console.WriteLine("Possible");
+                }
+                else
+                {
+                    Console.WriteLine("Impossible");
+                }
+            }
+        }
+    }
+}
---
+++ kattis_cs/Sorttwonumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis_cs
+{
+    class Sorttwonumbers
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine().Split();
+            if (int.Parse(numbers[0]) > int.Parse(numbers[1]))
+            {
+                Console.WriteLine(numbers[1] + " " + numbers[0]);
+            }
+            else
+            {
+                Console.WriteLine(numbers[0] + " " + numbers[1]);
+            }
+        }
+    }
+}
