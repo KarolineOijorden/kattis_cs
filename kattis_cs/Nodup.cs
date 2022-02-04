7e9199805e12375575e819a3ae39064e77a48692
Karoline Øijorden <karolineoij@gmail.com>
2022-02-04 13:33:37 +0100

    add Nodup.cs

---
+++ kattis_cs/Nodup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis_cs
+{
+    class Nodup
+    {
+        static void Main(string [] args)
+        {
+            string [] words = Console.ReadLine().Split();
+            string result = "yes";
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = i + 1; j < words.Length; j++)
+                {
+                    if (words[i] == words[j])
+                    {
+                        result = "no";
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
