b8e7646cb6f9476356bfbba7874b6305ea99d012
Karoline Øijorden <karolineoij@gmail.com>
2022-02-04 12:31:08 +0100

    add Reversebinary

---
+++ kattis_cs/Reversebinary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis_cs
+{
+    class Reversebinary
+    {
+        public static void Main(string [] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            string binaryNumber = "";
+            while (number > 0)
+            {
+                int remainder = number % 2;
+                binaryNumber += remainder.ToString();
+                number /= 2;
+            }
+            double newNumber = 0;
+            for (int i = 0; i < binaryNumber.Length; i++)
+            {
+                newNumber += Convert.ToInt32(Char.GetNumericValue(binaryNumber[i]) * Math.Pow(2, binaryNumber.Length - i - 1));
+            }
+            Console.WriteLine(newNumber);
+        }
+    }
+}
