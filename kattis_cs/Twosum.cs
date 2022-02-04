d89c8eda8640e7d5d7e7bd22e94515b8a5ad39c9
Karoline Øijorden <karolineoij@gmail.com>
2022-02-04 12:51:53 +0100

    add Twosum.cs

---
+++ kattis_cs/Twosum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis_cs
+{
+    class Twosum
+    {
+        static void Main(string [] args)
+        {
+            string [] numbers = Console.ReadLine().Split();
+            Console.WriteLine(int.Parse(numbers[0]) + int.Parse(numbers[1]));
+        }
+    }
+}
