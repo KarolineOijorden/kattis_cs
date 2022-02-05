c47215e7d3150db3c2ed3f4629f0b22f4dc1d011
Karoline Øijorden <karolineoij@gmail.com>
2022-02-05 09:21:27 +0100

    merged Sorttwonumbers

--- kattis_cs/Sorttwonumbers.cs
+++ kattis_cs/Sorttwonumbers.cs
@@ -5,14 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace kattis_cs
-{
-    class using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace kattis_cs
     {
         class Sorttwonumbers
         {
@@ -30,18 +22,3 @@ namespace kattis_cs
             }
         }
     }
-
-    {
-        static void Main(string [] args)
-        {
-            string [] numbers = Console.ReadLine().Split();
-            if (int.Parse(numbers[0]) > int.Parse(numbers[1]))
-            {
-                Console.WriteLine(numbers[1] + " " + numbers[0]);
-            } else
-            {
-                Console.WriteLine(numbers[0] + " " + numbers[1]);
-            }
-        }
-    }
-}
