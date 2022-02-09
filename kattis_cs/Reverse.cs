e136fefeee7aaaded60ddd6f2600eca0f44eb8d5
Karoline Øijorden <karolineoij@gmail.com>
2022-02-09 15:40:52 +0100

    Add Reverse

---
+++ kattis_cs/Reverse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis_cs
+{
+    class Reverse
+    {
+        static void Main(String[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] numbers = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = Console.ReadLine();
+            }
+            for (int j = n - 1; j >= 0; j--)
+            {
+                Console.WriteLine(numbers[j]);
+            }
+        }
+    }
+}
