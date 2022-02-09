8da8d9204f59f23f1b4fd84db33d5aa14d82af5c
Karoline Øijorden <karolineoij@gmail.com>
2022-02-09 15:43:05 +0100

    Add Quadrant

---
+++ kattis_cs/Quadrant.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis_cs
+{
+    class Quadrant
+    {
+        static void Main(string[] args)
+        {
+            bool xPositive = int.Parse(Console.ReadLine()) > 0;
+            bool yPositive = int.Parse(Console.ReadLine()) > 0;
+            int quadrant = -1;
+            if (xPositive && yPositive)
+            {
+                quadrant = 1;
+            }
+            else if (!xPositive && yPositive)
+            {
+                quadrant = 2;
+            }
+            else if (!xPositive && !yPositive)
+            {
+                quadrant = 3;
+            }
+            else if (xPositive && !yPositive)
+            {
+                quadrant = 4;
+            }
+            Console.WriteLine(quadrant);
+        }
+    }
+}
