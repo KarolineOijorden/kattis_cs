1b7317274586ea7ce5b0cda420978434f7e22b26
Karoline Øijorden <karolineoij@gmail.com>
2022-02-09 15:43:59 +0100

    Add Qaly

---
+++ kattis_cs/Qaly.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis_cs
+{
+    class Qaly
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double result = 0;
+            for (int i = 0; i < n; i++)
+            {
+                string[] lifeQuality = Console.ReadLine().Split();
+                result += Convert.ToDouble(lifeQuality[0]) * Convert.ToDouble(lifeQuality[1]);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
