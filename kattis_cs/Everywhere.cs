8525c3938308e1692ab2b9845e2880aa788bb3b0
Karoline Øijorden <karolineoij@gmail.com>
2022-02-09 15:51:14 +0100

    Add Everywhere

---
+++ kattis_cs/Everywhere.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis_cs
+{
+    class Everywhere
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < cases; i++)
+            {
+                int nCities = int.Parse(Console.ReadLine());
+                List<string> cities = new List<string>(nCities);
+                for (int j = 0; j < nCities; j++)
+                {
+                    cities.Add(Console.ReadLine());
+                }
+                Console.WriteLine(cities.Distinct().Count());
+            }
+        }
+    }
+}
