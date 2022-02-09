e07fa55c91f8c714a4c1936053435b5ab80fd0d9
Karoline Øijorden <karolineoij@gmail.com>
2022-02-09 15:42:04 +0100

    Add Addtwonumbers

---
+++ kattis_cs/Addtwonumbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis_cs
+{
+    class Addtwonumbers
+    {
+        static void Main(String[] args)
+        {
+            string[] numbers = Console.ReadLine().Split();
+            Console.WriteLine(int.Parse(numbers[0]) + int.Parse(numbers[1]));
+        }
+    }
+}
