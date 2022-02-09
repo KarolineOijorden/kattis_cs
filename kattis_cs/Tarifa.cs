b85aa50f3d51e6826da43092155100a8dbcb08f1
Karoline Øijorden <karolineoij@gmail.com>
2022-02-09 15:57:20 +0100

    Add Tarifa

---
+++ kattis_cs/Tarifa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis_cs
+{
+    class Tarifa
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            int result = x * (n + 1);
+            for (int i = 0; i < n; i++)
+            {
+                result -= int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
