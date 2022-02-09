9720ff5b131cd95f9e922f3c6571b335db866b6b
Karoline Øijorden <karolineoij@gmail.com>
2022-02-09 15:16:37 +0100

    Add Nsum

---
+++ kattis_cs/Nsum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis_cs
+{
+    class Nsum
+    {
+        static void Main(string [] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int [] values = Array.ConvertAll(Console.ReadLine().Split(), s => int.Parse(s));
+            int result = 0;
+            for (int i = 0; i < n; i++)
+            {
+                result += values[i];
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
