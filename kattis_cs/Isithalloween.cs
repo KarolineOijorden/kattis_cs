403a3407d4cec0f0de4054e698dddddc919510cf
Karoline Øijorden <karolineoij@gmail.com>
2022-02-09 15:39:53 +0100

    Add Isithalloween

---
+++ kattis_cs/Isithalloween.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis_cs
+{
+    class Isithalloween
+    {
+        static void Main(String[] args)
+        {
+            string[] date = Console.ReadLine().Split();
+            if (date[0] == "OCT" && date[1] == "31")
+            {
+                Console.WriteLine("Yup");
+            }
+            else if (date[0] == "DEC" && date[1] == "25")
+            {
+                Console.WriteLine("Yup");
+            }
+            else
+            {
+                Console.WriteLine("nope");
+            }
+        }
+    }
+}
