8c35c0dedc5bdf30246c3f43d732bca02e875357
Karoline Øijorden <karolineoij@gmail.com>
2022-02-09 15:38:41 +0100

    Add different

---
+++ kattis_cs/Different.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis_cs
+{
+    class Different
+    {
+        static void Main(String[] args)
+        {
+            string line;
+            while (!string.IsNullOrEmpty(line = Console.ReadLine()))
+            {
+                string[] values = line.Split();
+                Console.WriteLine(Math.Abs(Convert.ToInt64(values[0]) - Convert.ToInt64(values[1])));
+            }
+        }
+    }
+}
