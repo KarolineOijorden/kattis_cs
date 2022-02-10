ee0ffb7a1d831884c2c165047543601127ac8a04
Karoline Øijorden <karolineoij@gmail.com>
2022-02-10 16:16:55 +0100

    add triarea

---
+++ kattis_cs/triarea.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis_cs
+{
+    class triarea
+    {
+        static void Main(string [] args)
+        {
+            double [] numbers = Array.ConvertAll(Console.ReadLine().Split(), s => Convert.ToDouble(s));
+            Console.Write(numbers[0] * numbers[1]/ 2);
+        }
+    }
+}
