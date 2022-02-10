f3514defddd543b11c227e0ff03cc4576bf43c4e
Karoline Øijorden <karolineoij@gmail.com>
2022-02-10 17:00:08 +0100

    add digitswap

---
+++ kattis_cs/digitswap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis_cs
+{
+    class digitswap
+    {
+        static void Main(string [] args)
+        {
+            string input = Console.ReadLine();
+            Console.Write(input[1]);
+            Console.Write(input[0]);
+        }
+    }
+}
