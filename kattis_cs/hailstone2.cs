eba0c0afeaec27f586f1e429cde356421f3e1890
Karoline Øijorden <karolineoij@gmail.com>
2022-02-10 17:20:01 +0100

    add hailstone2

---
+++ kattis_cs/hailstone2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis_cs
+{
+    class hailstone2
+    {
+        static void Main(string [] args)
+        {
+            long n = Convert.ToInt64(Console.ReadLine());
+            List<long> sequence = new List<long>();
+            sequence.Add(n);
+            while (sequence.Last() != 1)
+            {
+                long a = sequence.Last();
+                if (a % 2 == 0)
+                {
+                    sequence.Add(a/2);
+                } else
+                {
+                    sequence.Add(3 * a + 1);
+                }
+            }
+            Console.WriteLine(sequence.Count);
+        }
+    }
+}
