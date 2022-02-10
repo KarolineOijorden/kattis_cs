ae0eff342fc6ffcd6d2688788f26245f05b246eb
Karoline Øijorden <karolineoij@gmail.com>
2022-02-10 14:45:06 +0100

    Add sumkindofproblem

---
+++ kattis_cs/sumkindofproblem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis_cs
+{
+    class sumkindofproblem
+    {
+        public static void Main(string [] args)
+        {
+            int p = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= p; i++)
+            {
+                string [] input = Console.ReadLine().Split();
+                int n = int.Parse(input[1]);
+                int s3 = n * (n + 1);
+                int s2 = n * n;
+                int s1 = s3 / 2;
+
+                Console.Write(i + " " + s1 + " " + s2 + " " + s3);
+                Console.WriteLine();
+            }
+        }
+    }
+}
