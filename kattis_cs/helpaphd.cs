9981a957d07f987ddf60b010fd6f7b14dbff0f4c
Karoline Øijorden <karolineoij@gmail.com>
2022-02-11 10:51:39 +0100

    Add helpaphd

---
+++ kattis_cs/helpaphd.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis_cs
+{
+    class helpaphd
+    {
+        static void Main(string [] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                if (input[0] == 'P')
+                {
+                    Console.WriteLine("skipped");
+                } else
+                {
+                    string[] values = input.Split('+');
+                    Console.WriteLine(int.Parse(values[0]) + int.Parse(values[1]));
+                }
+            }
+        }
+    }
+}
