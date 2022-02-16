65c6c49447bf88c1836b769b6966a720cebe78c4
Karoline Øijorden <karolineoij@gmail.com>
2022-02-16 16:20:06 +0100

    add filip

---
+++ kattis_cs/filip.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis_cs
+{
+    class filip
+    {
+        static void Main(string [] args)
+        {
+            string [] input = Console.ReadLine().Split();
+            string x = "";
+            string y = "";
+            for (int i = 2; i >= 0; i--)
+            {
+                x += input[0][i];
+                y += input[1][i];
+            }
+            if (int.Parse(x) > int.Parse(y))
+            {
+                Console.WriteLine(x);
+            } else
+            {
+                Console.WriteLine(y);
+            }
+        }
+    }
+}
