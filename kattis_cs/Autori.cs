1b0facd3c159342939a3eaf2c68c331cabed64e9
Karoline Øijorden <karolineoij@gmail.com>
2022-02-16 16:30:30 +0100

    add Autori

---
+++ kattis_cs/Autori.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis_cs
+{
+    class Autori
+    {
+        static void Main(string [] args)
+        {
+            string [] names = Console.ReadLine().Split('-');
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.Write(names[i][0]);
+            }
+        }
+    }
+}
