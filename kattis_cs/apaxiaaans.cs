5512474602b5b28799ef033e4f4114634c7dd49a
Karoline Øijorden <karolineoij@gmail.com>
2022-02-16 10:34:16 +0100

    add apaxiaaans

---
+++ kattis_cs/apaxiaaans.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis_cs
+{
+    class apaxiaaans
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string result = name[0].ToString();
+            for (int i = 1; i < name.Length; i++)
+            {
+                if (name[i] != name[i - 1])
+                {
+                    result = result + name[i].ToString();
+                }
+            }
+            Console.Write(result);
+        }
+    }
+}
---
+++ kattis_cs/oddmanout.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis_cs
+{
+    class oddmanout
+    {
+        static void Main(string [] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int g = int.Parse(Console.ReadLine());
+                string [] guests = Console.ReadLine().Split();
+               // string [] guests.Distinct().ToArray();
+               // guests.
+            }
+        }
+    }
+}
