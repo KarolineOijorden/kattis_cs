719a6f92b42eafb52ca89de6d0dfb9466fe27581
Karoline Øijorden <karolineoij@gmail.com>
2022-02-17 09:32:36 +0100

    add Aaah

---
+++ kattis_cs/Aaah.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis_cs
+{
+    class Aaah
+    {
+        static void Main(string [] args)
+        {
+            string a = Console.ReadLine();
+            if (a.Length >= Console.ReadLine().Length)
+            {
+                Console.WriteLine("go");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
